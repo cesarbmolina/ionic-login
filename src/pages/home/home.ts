c31554f88f232e6d048b97841c5b709a75ffe98a
Cesar Molina <cesar@multiplica.com>
2018-07-13 01:35:46 -0500

    feat(home): ingresando datos compartidos simples

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,77 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
 import { AuthService } from '../../service/auth.service';
-
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthService) {}
+  tasksRef: AngularFireList<any>;
+  tasks: Observable;
+
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public auth: AuthService,
+    public database: AngularFireDatabase
+  ) {
+    this.tasksRef = this.database.list('tasks');
+    this.tasks = this.tasksRef.snapshotChanges()
+    .pipe(
+      map(changes => {
+        console.log(changes);
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      })
+    );
+
+  }
+
+  createTask(){
+    let newTaskModal = this.alertCtrl.create({
+      title: 'New Task',
+      message: "Enter a title for your new task",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.tasksRef.push({
+              title: data.title,
+              done: false
+            });
+          }
+        }
+      ]
+    });
+    newTaskModal.present( newTaskModal );
+  }
+
+  updateTask( task ){
+    this.tasksRef.update( task.key,{
+      title: task.title,
+      done: !task.done
+    });
+  }
+
+  removeTask( task ){
+    console.log( task );
+    this.tasksRef.remove( task.key );
+  }
 
   login() {
     this.auth.signOut();
